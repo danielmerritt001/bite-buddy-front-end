a7bafd7f26ccd885fc9641af037b60231eef58d4
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-15 10:07:52 -0400

    photos added to recipe list

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -4,8 +4,12 @@ import styles from './RecipeCard.module.css'
 const RecipeCard = (props) => {
   const recipe = props.recipeSingle.recipe
   console.log(recipe)
+  console.log(recipe.images.SMALL.url)
   return (
     <article className={styles.container}>
+      <div>
+        <img src={recipe.images.SMALL.url} alt={recipe.label} />
+      </div>
       <div>
         {recipe.label}
       </div>
