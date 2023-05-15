f856e52c9e2d89b6c68b02f1b4aeeb054be780d2
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-15 10:34:12 -0400

    add author to recipe card

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -4,7 +4,6 @@ import styles from './RecipeCard.module.css'
 const RecipeCard = (props) => {
   const recipe = props.recipeSingle.recipe
   console.log(recipe)
-  console.log(recipe.images.SMALL.url)
   return (
     <article className={styles.container}>
       <div>
@@ -12,6 +11,9 @@ const RecipeCard = (props) => {
       </div>
       <div>
         {recipe.label}
+      </div>
+      <div>
+        {recipe.source}
       </div>
     </article>
     )
