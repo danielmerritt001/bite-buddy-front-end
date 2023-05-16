63bb0922333f39976e4335f7d5a77888b1741904
EleahBurman <eleahjburman@gmail.com>
2023-05-16 14:30:43 -0400

    words in nav bar correct color and spaceand logout floats

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,32 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
+//css
+import styles from './NavBar.module.css'
+
 const NavBar = ({ user, handleLogout }) => {
   return (
     <nav>
       {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="/recipes">Recipes</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/boards">Boards</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-        </ul>
+
+          <main className={`${styles.container}`}>
+          <div className={`${styles.everythingbutlogout}`}>
+              <span className={`${styles.navlinks}`}><NavLink to="/profiles">Profiles</NavLink></span>
+              <span className={`${styles.navlinks}`}><NavLink to="/recipes">Recipes</NavLink></span>
+              <span className={`${styles.navlinks}`}><NavLink to="/boards">Boards</NavLink></span>
+              {/* will put change password into profile */}
+              {/* <span className={`${styles.navlinks}`}><NavLink to="/auth/change-password">Change Password</NavLink></span> */}
+          </div>
+            <div className={`${styles.logout}`}>
+              <span className={`${styles.navlinks}`} id={`${styles.logout}`}><NavLink to="" onClick={handleLogout}>Log Out</NavLink></span>
+            </div>
+          </main>
+          
       :
-        <ul>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-        </ul>
+          <div className={`${styles.allnavlinks}`}>
+            <span className={`${styles.navlinks}`}><NavLink to="/auth/login">Log In</NavLink></span>
+            <span className={`${styles.navlinks}`}><NavLink to="/auth/signup">Sign Up</NavLink></span>
+          </div>
       }
     </nav>
   )
