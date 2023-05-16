c31c813251a9ae5545ae6d97833a6a211cc656ff
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-16 10:55:47 -0400

    profile path complete

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -4,11 +4,26 @@ import BoardCard from '../../components/BoardCard/BoardCard'
 //css
 import styles from './ProfileCard.module.css'
 
-const ProfileCard = ({profile, handleGetRecipe}) => {
+const ProfileCard = ({ profile }) => {
+  if(!(profile.boards.length === 0)) {
+    return (  
+      <main className={`${styles.container} ${styles.main}`}>
+      <h1>Hello, I'm the Profile Card</h1>
+      <h3>Again, I'm {profile.name}</h3>
+      {profile.boards.map(board => 
+        <>
+          <BoardCard board={board} /> 
+        </>
+        )
+      }
+      
+    </main>
+    )
+  }
   return (  
     <main className={`${styles.container} ${styles.main}`}>
     <h1>Hello, I'm the Profile Card</h1>
-    <BoardCard profile={profile} handleGetRecipe={handleGetRecipe}/>
+    <h3>Again, I'm {profile.name}</h3>
   </main>
   )
 }
