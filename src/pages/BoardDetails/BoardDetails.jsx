c7840e39a40fa419282d94e9eb3325102a83a342
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-16 13:42:51 -0400

    board fetch functionality

--- src/pages/BoardDetails/BoardDetails.jsx
+++ src/pages/BoardDetails/BoardDetails.jsx
@@ -11,7 +11,7 @@ import * as boardService from '../../services/boardService'
 //css
 import styles from './BoardDetails.module.css'
 
-const BoardDetails = (props) => {
+const BoardDetails = () => {
   const { boardId } = useParams()
   const [board, setBoard] = useState(null)
 
@@ -22,14 +22,18 @@ const BoardDetails = (props) => {
     }
     fetchBoard()
   }, [boardId])
-
-  console.log(boardId)
+if(board){
   return (  
     <main className={`${styles.container} ${styles.main}`}>
       <h1>Hello, I'm the Board Details</h1>
+      <div id={board.bgColor}>
+        <div>{board.title}</div>
+        <div>by {board.author.name}</div>
+      </div>
       {/* <RecipeCard /> */}
     </main>
   )
+}
 }
 
 export default BoardDetails
