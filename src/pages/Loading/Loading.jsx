7e95a5da5d0286771c02c496ec3def3fe5fbcba2
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-17 08:43:32 -0400

    loading page

---
+++ src/pages/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+//css
+import styles from './Loading.module.css'
+
+const Loading = () => {
+  return (
+    <main className={styles.container}>
+      <h3>...Loading</h3>
+    </main>
+  )
+}
+
+export default Loading
