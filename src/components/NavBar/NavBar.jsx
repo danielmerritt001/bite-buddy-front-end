724dbecd88adc83773945199741ea0caf437f576
EleahBurman <eleahjburman@gmail.com>
2023-05-17 14:44:55 -0400

    Fix nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,21 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
+//assets
+import home from '../../assets/icons/bitebuddyicon.png'
+
+import profiles from '../../assets/icons/profilesicon.png'
+
+import recipes from '../../assets/icons/recipesicon.png'
+
+import boards from '../../assets/icons/boardsicon.png'
+
+import logout from '../../assets/icons/logouticon.png'
+
+import login from '../../assets/icons/loginicon.png'
+
+import signup from '../../assets/icons/signupicon.png'
+
 //css
 import styles from './NavBar.module.css'
 
@@ -12,25 +27,25 @@ const NavBar = ({ user, handleLogout }) => {
           <div className={`${styles.everythingbutlogout}`}>
             <span className={`${styles.navlinks}`}>
               <NavLink to="/">
-                <img src="src/assets/images/bitebuddyicon.png" width="32" alt="home" />
+                <img src={home} width="32" alt="home" />
                 <span className={`${styles.label}`}>Home</span>
               </NavLink>
             </span>
             <span className={`${styles.navlinks}`}>
               <NavLink to="/profiles">
-                <img src="src/assets/images/profilesicon.png" width="32" alt="profiles" />
+                <img src={profiles} width="32" alt="profiles" />
                 <span className={`${styles.label}`}>Profiles</span>
               </NavLink>
             </span>
             <span className={`${styles.navlinks}`}>
               <NavLink to="/recipes">
-                <img src="src/assets/images/recipesicon.png" width="32" alt="images" />
+                <img src={recipes} width="32" alt="images" />
                 <span className={`${styles.label}`}>Recipes</span>
               </NavLink>
             </span>
             <span className={`${styles.navlinks}`}>
               <NavLink to="/boards">
-                <img src="src/assets/images/boardsicon.png" width="32" alt="boards" />
+                <img src={boards} width="32" alt="boards" />
                 <span className={`${styles.label}`}>Boards</span>
               </NavLink>
             </span>
@@ -40,7 +55,7 @@ const NavBar = ({ user, handleLogout }) => {
           <div className={`${styles.logout}`}>
             <span className={`${styles.navlinks}`} id={`${styles.logout}`}>
               <NavLink to="" onClick={handleLogout}>
-                <img src="src/assets/images/logouticon.png" width="32" alt="logout" />
+                <img src={logout} width="32" alt="logout" />
                 <span className={`${styles.label}`}>Logout</span>
               </NavLink>
             </span>
@@ -50,13 +65,13 @@ const NavBar = ({ user, handleLogout }) => {
         <div className={`${styles.allnavlinks}`}>
           <span className={`${styles.navlinks}`}>
             <NavLink to="/auth/login">
-              <img src="src/assets/images/loginicon.png" width="32" alt="login" />
+              <img src={login} width="32" alt="login" />
               <span className={`${styles.label}`}>Login</span>
             </NavLink>
           </span>
           <span className={`${styles.navlinks}`}>
             <NavLink to="/auth/signup">
-              <img src="src/assets/images/signupicon.png" width="32" alt="signup" />
+              <img src={signup} width="32" alt="signup" />
               <span className={`${styles.label}`}>Signup</span>
             </NavLink>
           </span>
