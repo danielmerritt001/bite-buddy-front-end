bec8043074530d59012b51a48236c5c74c7cbd7d
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-17 19:40:43 -0400

    fix bugs in profile and recipe lists

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -23,11 +23,12 @@ const ProfileDetails = () => {
     fetchProfile()
   }, [profileId])
 
-console.log(profile)
 if(profile){
   return (
     <main className={`${styles.container} ${styles.main}`}>
       <h1>Hello, I'm the profile details</h1>
+      <h3>Profile: {profile.name}</h3>
+      <h3>Prounouns:{profile.pronouns}</h3>
       {profile.boards.map((board)=>(
           (board ? <BoardCard key={board._id} board={board}/> : <h3 key={board._id}>No Recipes Yet!</h3>)
           ))}
