10a8ba2704958b7b04ce5c46922a2139fc395c29
EleahBurman <eleahjburman@gmail.com>
2023-05-18 10:11:23 -0400

    css and divs in place for landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,38 @@
+// npm modules
+import { Link } from 'react-router-dom'
+
 // css
 import styles from './Landing.module.css'
 
+//assets
+import cookingblue from '../../assets/icons/familycookingblue.png'
+
+import cookingpink from '../../assets/icons/familycookingpink.png'
+
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
-    </main>
+    <>
+      <main className={`${styles.square} ${styles.container}`}>
+        <div className={`${styles.square} ${styles.squareone}`}>
+          <h1>Bite Buddy</h1>
+          <h2>Hey {user ? user.name : 'friend'}! </h2>
+          <h2>Bake your day better with these recipes!</h2>
+        </div>
+        <div className={`${styles.square} ${styles.squaretwo}`}>
+          <img src={cookingblue} width='200' height='200'/>
+        </div>
+        <div className={`${styles.square} ${styles.squarethree}`}>
+          <h1>Bite Buddy</h1>
+          <h2>Sign in for some spec-taco-lar recipes</h2>
+          <Link to="/auth/login">
+            <button>Sign-In</button>
+          </Link>
+        </div>
+        <div className={`${styles.square} ${styles.squarefour}`}>
+          <img src={cookingpink} width='200' height='200'/>
+        </div>
+      </main>
+    </>
   )
 }
 
