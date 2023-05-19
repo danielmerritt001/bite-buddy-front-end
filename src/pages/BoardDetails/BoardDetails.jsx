25a3c7f47dd1bf18475c445921a31d280c14df60
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-19 10:06:03 -0400

    bug fixes

--- src/pages/BoardDetails/BoardDetails.jsx
+++ src/pages/BoardDetails/BoardDetails.jsx
@@ -1,6 +1,6 @@
 //npm modules
 import { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 
 // components
 import RecipeCard from '../../components/RecipeCard/RecipeCard'
@@ -10,10 +10,13 @@ import * as boardService from '../../services/boardService'
 
 //css
 import styles from './BoardDetails.module.css'
+import { getAllProfiles } from '../../services/profileService'
 
 const BoardDetails = (props) => {
   const { boardId } = useParams()
-  const [board, setBoard] = useState(null)
+  const [board, setBoard] = useState({})
+  const [recipes, setRecipes] = useState(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchBoard = async () => {
@@ -23,18 +26,31 @@ const BoardDetails = (props) => {
     fetchBoard()
   }, [boardId])
 
-  if (board) {
+  useEffect(() => {
+    setRecipes(board.recipes)
+  }, [board])
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    navigate(`/boards/${boardId}/edit`)
+  }
+
+  const handleRemoveFromBoard = async (recipe) => {
+    const foodId = recipe.uri.split('recipe_')[1]
+    const removedRecipe = await boardService.removeRecipeFromBoard(boardId, foodId)
+    setRecipes(recipes.filter((recipe) => recipe.foodId !== removedRecipe.foodId))
+  }
+  
+  // const handleRemoveFromBoard = (recipe) => {
+  //   console.log(recipe.foodId)
+  //   navigate('/boards')
+  // }
+
+  if (board && recipes) {
+    console.log(recipes)
     return (
       <main className={`${styles.main}`}>
         <div className={`${styles.boarddetailscontainer}`} >
-          {board.author._id === props.user?.profile &&
-            <>
-              <Link to={`/boards/${boardId}/edit`} state={board}>Edit</Link>
-              <button onClick={() => props.handleDeleteBoard(boardId)}>
-                Delete
-              </button>
-            </>
-          }
           <div id={board.bgColor}>
             <div className={`${styles.boarddetailstitle}`}>
               {board.title}
@@ -42,9 +58,25 @@ const BoardDetails = (props) => {
             <div className={`${styles.boarddetailsauthor}`} >
               by {board.author.name}
             </div>
+            {board.author._id === props.user?.profile &&
+              <>
+                <Link to={`/boards/${boardId}/edit`} state={board}>Edit</Link>
+                <button onClick={handleSubmit}>Edit</button>
+                <button onClick={() => props.handleDeleteBoard(boardId)}>
+                  Delete
+                </button>
+              </>
+            }
           </div>
           {board.recipes.map((recipe, idx) => (
-            <RecipeCard key={idx} recipe={recipe} />
+            <div key={idx} >
+              {board.author._id === props.user?.profile &&
+                    <button  onClick={()=> handleRemoveFromBoard(recipe)}>
+                      Remove
+                    </button>
+              }
+              <RecipeCard recipe={recipe} />
+            </div>
           ))}
         </div>
       </main>
