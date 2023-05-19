10b63b33f7dab50059fabb55b69bac9fd828d8f9
EleahBurman <eleahjburman@gmail.com>
2023-05-18 22:36:49 -0400

    Add classnames and spacing

--- src/pages/NewBoard/NewBoard.jsx
+++ src/pages/NewBoard/NewBoard.jsx
@@ -20,34 +20,42 @@ const NewBoard = (props) => {
   }
 
   return (
-    <main className={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='title-input'>Title</label>
-        <input
-          required
-          type='text'
-          name='title'
-          id='title-input'
-          value={formData.title}
-          placeholder='Title'
-          onChange={handleChange}
-        />
-        <label htmlFor='bgColor-input'>Background Color</label>
-        <select
-          required
-          name='bgColor'
-          id='bgColor-input'
-          value={formData.bgColor}
-          onChange={handleChange}
-        >
-          <option value='blue'>Cornflower Blue</option>
-          <option value='gray'>Whisper Gray</option>
-          <option value='green'>Cascade Green</option>
-          <option value='pink'>Blush Pink</option>
-          <option value='purple'>Lavender</option>
-        </select>
-        <button type='submit'>SUBMIT</button>
-      </form>
+    <main>
+      <div className={`${styles.newboardcontainer}`}>
+        <form onSubmit={handleSubmit}>
+          <div className={`${styles.titleboard}`}>
+            <label htmlFor='title-input'>Title</label>
+            <input
+              required
+              type='text'
+              name='title'
+              id='title-input'
+              value={formData.title}
+              placeholder='Title'
+              onChange={handleChange}
+            />
+          </div>
+          <div className={`${styles.backgroundcolor}`}>
+          <label htmlFor='bgColor-input'>Background Color</label>
+          <select
+            required
+            name='bgColor'
+            id='bgColor-input'
+            value={formData.bgColor}
+            onChange={handleChange}
+          >
+            <option value='blue'>Cornflower Blue</option>
+            <option value='gray'>Whisper Gray</option>
+            <option value='green'>Cascade Green</option>
+            <option value='pink'>Blush Pink</option>
+            <option value='purple'>Lavender</option>
+          </select>
+          </div>
+          <div className={`${styles.submitbutton}`}>
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+      </div>
     </main>
   )
 }
